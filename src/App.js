271177ab5bead6f9b118fd9379d8b28b6d31589b
harshitha707 <53364952+harshitha707@users.noreply.github.com>
2024-11-12 22:17:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import { useState } from 'react'
+
+function TodoList () {
+  const [todos, setTodos] = useState([])
+  const [inputValue, setInputValue] = useState('')
+
+function handleChange(e){
+  setInputValue(e.target.value)
 }
 
-export default App;
+function handleSubmit(e){
+  e.preventDefault()
+  setTodos([...todos, inputValue])
+  setInputValue('')
+}
+
+function handleDelete(index){
+  const newTodos = [...todos]
+  newTodos.splice(index, 1)
+  setTodos(newTodos)
+}
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <form>
+        <input type='text' value={inputValue} onChange={handleChange}/>
+        <button onClick={handleSubmit}>Add Todo</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}
+          <button onClick={() =>handleDelete(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default TodoList;
+
